796a827ac910a2759fb04ae087a4dc5b7c618bbf
Nick <noliveira95@outlook.com>
2024-03-08 18:35:46 -0800

    feat: start navigation

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cc/classes/card_class.dart';
 import 'package:flutter_cc/core/constants.dart';
+import 'package:flutter_cc/pages/learn_more.dart';
 
 class CardWidget extends StatelessWidget {
   const CardWidget({
@@ -11,27 +12,39 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.only(
-          top: kDouble10,
-          bottom: kDouble10,
-        ),
-        width: double.infinity,
-        child: Column(
-          children: [
-            Image.asset(content.image),
-            Text(
-              content.title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const LearnMorePage();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.only(
+            top: kDouble10,
+            bottom: kDouble10,
+          ),
+          width: double.infinity,
+          child: Column(
+            children: [
+              Image.asset(content.image),
+              Text(
+                content.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              content.description ?? 'Learn more about ${content.title}',
-            ),
-          ],
+              Text(
+                content.description ?? 'Learn more about ${content.title}',
+              ),
+            ],
+          ),
         ),
       ),
     );
