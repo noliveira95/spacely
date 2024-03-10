7136ee85b8c13dc5e500749aceb2534942ec8a00
Nick <noliveira95@outlook.com>
2024-03-09 16:13:41 -0800

    fix: display favorites

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cc/core/constants.dart';
+import 'package:flutter_cc/models/card_class.dart';
+import 'package:flutter_cc/models/favorites_model.dart';
+import 'package:flutter_cc/widgets/card_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_cc/models/planet_info.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
@@ -13,8 +19,37 @@ class FavoritesPage extends StatelessWidget {
         ),
         backgroundColor: Colors.indigo,
       ),
-      body: const Center(
-        child: Text('Favorites'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Consumer<FavoritesModel>(
+            builder: (context, favoritesModel, child) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(
+                  kDouble20,
+                  kDouble20,
+                  kDouble20,
+                  kDouble40,
+                ),
+                child: Column(children: [
+                  if (favoritesModel.favorites.isEmpty)
+                    const Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: kDouble40),
+                        child: Text('No favorites yet!'),
+                      ),
+                    ),
+                  for (var planet in favoritesModel.favorites)
+                    CardWidget(
+                      content: CardClass(
+                        title: planetInfo[planet]!.name,
+                        image: planetInfo[planet]!.image,
+                      ),
+                    ),
+                ]),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
