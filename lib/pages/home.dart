e66cabdb4c3a1ab1efb43bb0f416e903b6cfe563
Nick <noliveira95@outlook.com>
2024-03-09 16:16:18 -0800

    style: add padding

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cc/core/constants.dart';
 import 'package:flutter_cc/models/card_class.dart';
 import 'package:flutter_cc/widgets/card_widget.dart';
 import 'package:flutter_cc/models/planet_info.dart';
@@ -17,89 +18,92 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.indigo,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['mercury']!.name,
-                      image: planetInfo['mercury']!.image,
+        child: Padding(
+          padding: const EdgeInsets.all(kDouble10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['mercury']!.name,
+                        image: planetInfo['mercury']!.image,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['venus']!.name,
-                      image: planetInfo['venus']!.image,
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['venus']!.name,
+                        image: planetInfo['venus']!.image,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['earth']!.name,
-                      image: planetInfo['earth']!.image,
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['earth']!.name,
+                        image: planetInfo['earth']!.image,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['mars']!.name,
-                      image: planetInfo['mars']!.image,
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['mars']!.name,
+                        image: planetInfo['mars']!.image,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['jupiter']!.name,
-                      image: planetInfo['jupiter']!.image,
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['jupiter']!.name,
+                        image: planetInfo['jupiter']!.image,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['saturn']!.name,
-                      image: planetInfo['saturn']!.image,
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['saturn']!.name,
+                        image: planetInfo['saturn']!.image,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['uranus']!.name,
-                      image: planetInfo['uranus']!.image,
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['uranus']!.name,
+                        image: planetInfo['uranus']!.image,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: CardWidget(
-                    content: CardClass(
-                      title: planetInfo['neptune']!.name,
-                      image: planetInfo['neptune']!.image,
+                  Expanded(
+                    child: CardWidget(
+                      content: CardClass(
+                        title: planetInfo['neptune']!.name,
+                        image: planetInfo['neptune']!.image,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
